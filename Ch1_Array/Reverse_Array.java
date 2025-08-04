30637d0bde33e49f03276520b8f55e7246754ef3
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-08-04 23:43:52 +0530

    first commit

--- Ch1_Array/Reverse_Array.java
+++ Ch1_Array/Reverse_Array.java
@@ -1,5 +1,24 @@
 package Ch1_Array;
 
 public class Reverse_Array {
-    
+    public static void reverse(int nums[]) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+        for (int arr : nums)
+            System.out.println(arr);
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 5, 9, 3 };
+
+        reverse(nums);
+    }
 }
