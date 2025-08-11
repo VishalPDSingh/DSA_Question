985fb46e0da93db138213f622cb3ffc89cf1ccda
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-08-11 20:55:08 +0530

    first commit

--- Ch2_Searching/Leet1351BS.java
+++ Ch2_Searching/Leet1351BS.java
@@ -1,31 +1,28 @@
 package Ch2_Searching;
 
 public class Leet1351BS {
-    public static int negativeCount(int arr[][])
-    {
-        int i=0; 
-        int j = arr[0].length-1;
+    public static int negativeCount(int arr[][]) {
+        int i = 0;
+        int j = arr[0].length - 1;
         int count = 0;
 
-        while(i<arr.length && j>=0)
-        {
-            if(arr[i][j] < 0)
-            {
+        while (i < arr.length && j >= 0) {
+            if (arr[i][j] < 0) {
                 count++;
                 j--;
-            }else
-            {
+            } else {
                 i++;
             }
         }
         return count;
     }
+
     public static void main(String[] args) {
-        int arr[][] = {{4,3,2,-1},
-                       {3,2,1,-1},
-                       {1,1,-1,-2},
-                       {-1,-1,-2,-3}};
-        int ans = negativeCount(arr); 
-        System.out.println(ans); 
+        int arr[][] = { { 4, 3, 2, -1 },
+                { 3, 2, 1, -1 },
+                { 1, 1, -1, -2 },
+                { -1, -1, -2, -3 } };
+        int ans = negativeCount(arr);
+        System.out.println(ans);
     }
 }
