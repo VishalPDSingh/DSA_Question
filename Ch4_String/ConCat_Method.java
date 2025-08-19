81710eb483224244d6cf27fc5cc8b0fc082c62ad
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-08-19 23:01:27 +0530

    first commit

---
+++ Ch4_String/ConCat_Method.java
@@ -0,0 +1,14 @@
+package Ch4_String;
+
+public class ConCat_Method {
+    public static void main(String[] args) {
+        
+        String s1 = "JAVA";
+        String s2 = "Python";
+
+        String s3 = s1.concat(s2);
+        String s4 = s1.concat(s2);
+
+        
+    }
+}
