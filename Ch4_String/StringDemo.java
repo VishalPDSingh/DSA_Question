2377495ce83ad3193768147056f024a6a4ae8962
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-08-19 19:05:35 +0530

    first commit

--- Ch4_String/StringDemo.java
+++ Ch4_String/StringDemo.java
@@ -3,102 +3,119 @@ package Ch4_String;
 public class StringDemo {
     public static void main(String[] args) {
         
-        // method-1 to creating string in java
-        String s1= "Java"; // constant pool
-        String s2 = "Java";
-        if(s1 == s2)
-        {
-            System.out.println("refernces are equal");
-        }
-        else
-        {
-            System.out.println("Ref is unequal");
-        }
-        if(s1.equals(s2))
-        {
-            System.out.println("Value are equal");
-        }else
-        {
-            System.out.println("Value are not equal");
-        }
+       // method 1
+       String s1 = "JAVA";
+       String s2 = "JAVA";
+       if(s1 == s2)
+       {
+        System.out.println("Ref is equal");
+       }
+       else
+       {
+        System.out.println("Ref is not equal");
+       }
+       if(s1.equals(s2))
+       {
+        System.out.println("Value are equal");
+       }else
+       {
+        System.out.println("Not Equal");
+       }
+       System.out.println("<................................................>");
 
-        // method-2 
-        String str1 = "JAVA";
-        String str2 = new String("JAVA");
+       // Method 2
+       String str1 = "JAVA";
+       String str2 = new String("JAVA");
+       if(str1 == str2)
+       {
+        System.out.println("Ref is equal");
+       }else
+       {
+        System.out.println("Ref is not equal ");
+       }
+       if(str1.equals(str2))
+       {
+        System.out.println("Value are equal");
+       }else{
+       System.out.println("Value are not equal");
+       }
+       System.out.println("<................................................>");
 
-        if(str1 == str2)
-        {
-            System.out.println("Ref is equal");
-        }
-        else
-        {
-            System.out.println("Ref is not equal");
-        }
+       // method 3
+       String name1 = "java";
+       String name2 = "JAVA";
 
-        if(str1.equals(str2))
-        {
-            System.out.println("Value are equal");
-        }else
-        {
-            System.out.println("Value are not equal");
-        }
+        if(name1 == name2)
+       {
+        System.out.println("Ref is equal");
+       }else
+       {
+        System.out.println("Ref is not equal ");
+       }
+       if(name1.equals(name2))
+       {
+        System.out.println("Value are equal");
+       }else{
+       System.out.println("Value are not equal");
+       }
+       System.out.println("<................................................>");
 
-        // method -3
-        String st1 = new String("JAVA");
-        String st2 = new String("JAVA");
+       //method 4
+       String naam1 = new String("JAVA");
+       String naam2 = new String("JAVA");
 
-         if(st1 == st2)
-        {
-            System.out.println("Ref is equal");
-        }
-        else
-        {
-            System.out.println("Ref is not equal");
-        }
+        if(naam1 == naam2)
+       {
+        System.out.println("Ref is equal");
+       }else
+       {
+        System.out.println("Ref is not equal ");
+       }
+       if(naam1.equals(naam2))
+       {
+        System.out.println("Value are equal");
+       }else{
+       System.out.println("Value are not equal");
+       }
 
-        if(st1.equals(st2))
-        {
-            System.out.println("Value are equal");
-        }else
-        {
-            System.out.println("Value are not equal");
-        }
-        System.out.println("<-------------------------------->");
+       if(s1.equalsIgnoreCase(str2))
+       {
+        System.out.println("Value are  equal");
+       }
+       else
+       {
+        System.out.println("Value are not equal");
+       }
+       System.out.println("<................................................>");
 
-        // method -4
-        String ns1 = "Java";
-        String ns2= "JAVA";
+       // method 5
+        String x = new String("Java");
+       String y = new String("JAVA");
 
-         if(ns1 == ns2)
-        {
-            System.out.println("Ref is equal");
-        }
-        else
-        {
-            System.out.println("Ref is not equal");
-        }
+        if(x == y)
+       {
+        System.out.println("Ref is equal");
+       }else
+       {
+        System.out.println("Ref is not equal ");
+       }
+       if(x.equals(y))
+       {
+        System.out.println("Value are equal");
+       }else{
+       System.out.println("Value are not equal");
+       }
 
-        if(ns1.equals(ns2))
-        {
-            System.out.println("Value are equal");
-        }else
-        {
-            System.out.println("Value are not equal");
-        }
+       if(x.equalsIgnoreCase(y))
+       {
+        System.out.println("Value are  equal");
+       }
+       else
+       {
+        System.out.println("Value are not equal");
+       }
+       System.out.println("<................................................>");
 
-        if(ns1.equalsIgnoreCase(str2))
-        {
-            System.out.println("Value are equal");
-        }
-        else
-        {
-            System.out.println("Value are not equal ");
-        }        
-          System.out.println("<-------------------------------->");        
-         
-          // method-5
-          String x = "java";
-          String y = new String("JAVA");
-          
+       
     }
 }
