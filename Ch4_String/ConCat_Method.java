a13f65e6369a0f117fd74aae1519d055e39656c7
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-08-20 10:50:23 +0530

    first commit

--- Ch4_String/ConCat_Method.java
+++ Ch4_String/ConCat_Method.java
@@ -6,9 +6,15 @@ public class ConCat_Method {
         String s1 = "JAVA";
         String s2 = "Python";
 
-        String s3 = s1.concat(s2);
-        String s4 = s1.concat(s2);
+    //    String s3 = s1.concat(s2);
+       String s3 = "JAVA".concat("Python");
+        String s4 = "JAVA".concat("Python");
+       // String s4 = s1.concat(s2);
 
-        
+        if (s3 == s4) {
+            System.out.println("Ref is equal");
+        } else {
+            System.out.println("Ref is not equal");
+        }
     }
 }
