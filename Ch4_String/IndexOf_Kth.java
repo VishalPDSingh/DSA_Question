f186938ae65987449c10db9c9c7e8235190981f0
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-08-20 19:29:51 +0530

    first commit

--- Ch4_String/IndexOf_Kth.java
+++ Ch4_String/IndexOf_Kth.java
@@ -4,34 +4,31 @@ import java.util.Scanner;
 
 public class IndexOf_Kth {
 
-    public static int indexOf_kth(String s, char c, int k)
-    {
+    public static int indexOf_kth(String s, char c, int k) {
         int count = 0;
-        for(int i=0; i<s.length(); i++)
-        {
-            if(s.charAt(i)== c)
-            {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
                 count++;
-                if(count == k)
-                {
+                if (count == k) {
                     return i;
                 }
             }
         }
         return -1;
     }
+
     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of String: ");
         String str = sc.nextLine();
         System.out.println("Enter the value of charter");
-       char ch = sc.nextLine().charAt(0); // read charcter
+        char ch = sc.nextLine().charAt(0); // read charcter
 
-       System.out.println("Enter the postion of character: ");
-       int k = sc.nextInt();
+        System.out.println("Enter the postion of character: ");
+        int k = sc.nextInt();
 
-       System.out.println(indexOf_kth(str, ch, k));
-       sc.close();
+        System.out.println(indexOf_kth(str, ch, k));
+        sc.close();
     }
 }
