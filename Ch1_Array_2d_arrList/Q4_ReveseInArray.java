030cfaa730ec434a37ffb31fc473f9e0edd71782
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-09-19 19:04:32 +0530

    first commit

--- Ch1_Array_2d_arrList/Q4_ReveseInArray.java
+++ Ch1_Array_2d_arrList/Q4_ReveseInArray.java
@@ -1,6 +1,8 @@
 package Ch1_Array_2d_arrList;
 
 public class Q4_ReveseInArray {
+
+    // Method 1
     public static void revesrArray(int arr[])
     {
         int start = 0;
@@ -18,8 +20,25 @@ public class Q4_ReveseInArray {
             System.out.println(ans);
         }
     }
+
+    // Method 2 
+    public static void revesrArrayM2(int arr[])
+    {
+       // int n = arr.length-1;
+        int ar[] = new int[arr.length];
+        for(int i = 0; i<ar.length; i++)
+        {
+            ar[i] = arr[arr.length-1-i];
+        }
+        for(int ans:ar)
+        {
+            System.out.print(ans+ " ");
+        }
+    }
     public static void main(String[] args) {
         int arr[] = {10,20,30,15,70};
         revesrArray(arr);
+        System.out.println("--------------------------------");
+        revesrArrayM2(arr);
     }
 }
